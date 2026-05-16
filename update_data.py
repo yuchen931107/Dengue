ee269acfb250381b22bcce42a1662c70484b3acc
yuchen931107 <teano931107@gmail.com>
2026-05-17 02:51:51 +0800

    Update update_data.py

--- update_data.py
+++ update_data.py
@@ -1,6 +1,6 @@
 import datetime
 from get_cases_data import get_cases
-from get_latest_weatrer import get_weather
+from get_latest_weather import get_weather
 from get_invest_data import get_invest
 
 if __name__ == "__main__":
