9bc7bdc08d30ec1da47aebcd739ff49f7c8c222c
yuchen931107 <teano931107@gmail.com>
2026-05-17 11:42:21 +0800

    Update update_data.py

--- update_data.py
+++ update_data.py
@@ -2,24 +2,28 @@ import datetime
 from get_cases_data import get_cases
 from get_latest_weather_data import get_weather
 from get_invest_data import get_invest
+from get_population_data import get_population
 
 if __name__ == "__main__":
     print("開始更新資料")
     print("="*50)
     
     #1.病例數更新
-    print("(1/3)抓取最新確診病例\n")
+    print("(1/4)抓取最新確診病例\n")
     cases_df = get_cases()
     
     #2.weather更新
-    print("(2/3)抓取近期氣象觀測\n")
+    print("(2/4)抓取近期氣象觀測\n")
     get_weather()
     
     #3.調查資料更新
-    print("(3/3)抓取病媒蚊調查紀錄\n")
+    print("(3/4)抓取病媒蚊調查紀錄\n")
     mos_df = get_invest()
+
+    #4.人口數更新
+    print("(4/4)抓取病媒蚊調查紀錄\n")
+    get_population()
     
-    print("="*50)
     print("所有資料皆已更新完畢！")
 
 
