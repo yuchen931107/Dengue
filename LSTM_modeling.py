851db8ca40eb3ee59d57ab36ed5805b777c6e72b
yuchen931107 <teano931107@gmail.com>
2026-05-27 19:36:14 +0800

    Delete LSTM_modeling.py

--- LSTM_modeling.py
+++
@@ -1,7 +0,0 @@
-from LSTM_preprocessing import dengue_dataloader
-
-
-#讀取處理好的loader可修改參數window_size、batch_size、split_year
-train_loader,test_loader,weight,dim=dengue_dataloader(window_size=4,
-                                                      batch_size=64,
-                                                      split_year=2024)
