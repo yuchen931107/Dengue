2d18a3c98e95569abc24aaceae9317982eadc100
yuchen931107 <teano931107@gmail.com>
2026-06-04 19:09:27 +0800

    Update get_cases_data.py

--- get_cases_data.py
+++ get_cases_data.py
@@ -1,26 +1,16 @@
 import pandas as pd
 import requests
 import io
-import os
+from scraperapi import fetch_data
 
 def get_cases():
     #疾病管制署資料開放平台
     #https://data.cdc.gov.tw/dataset/dengue-daily-determined-cases-1998/resource/e868ae05-2381-44f2-9656-42292ef7e0c6
     url = "https://od.cdc.gov.tw/eic/Dengue_Daily.csv"
-    api_key = os.getenv("SCRAPERAPI_KEY")
-    print("正在透過 ScraperAPI 代理下載每日確診病例資料...")
-    
-    payload = {
-        'api_key': api_key,
-        'url': url,
-        'country_code': 'tw', 
-        'premium': 'true' 
-    }
+    print("正在透過下載每日確診病例資料...")
     
     try:
-        response = requests.get('http://api.scraperapi.com', params=payload, timeout=60)
-        response.raise_for_status()
-        response.encoding = 'utf-8-sig'
+        response = fetch_data(url)
         
         df = pd.read_csv(io.StringIO(response.text), low_memory=False)
         df.columns = df.columns.str.strip()
@@ -43,3 +33,4 @@ def get_cases():
         print("發生錯誤: ScraperAPI 連線超時，請稍後再試。")
     except Exception as e:
         print(f"發生錯誤: {e}")
+        
